f8a92e7c658eca26d576cc9ccd857d96e58f6be5
Idou-dev <idouyoussef87@gmail.com>
2024-07-01 04:13:48 -0700

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# Using Puppet to create a file in /tmp.
+
 file { '/tmp/school':
-ensure  => 'file',
-owner   => 'www-data',
-group   => 'www-data',
-mode    => '0744',
-content => 'I love Puppet',
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
